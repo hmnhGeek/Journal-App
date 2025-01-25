b452f2fb5f8150b141ef687c2cd0e4b811e108a7
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-25 20:44:32 +0530

    Added Lombok

--- src/main/java/com/himanshu/journalApp/entities/JournalEntry.java
+++ src/main/java/com/himanshu/journalApp/entities/JournalEntry.java
@@ -1,11 +1,11 @@
 package com.himanshu.journalApp.entities;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /*
 * This is a Plain Old Java Object (POJO).
@@ -14,42 +14,11 @@ import java.util.Date;
 * collection.
 * */
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
