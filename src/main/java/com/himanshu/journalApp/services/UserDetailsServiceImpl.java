34516281156076fe65fd1b016e2875b8c57f39ac
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-28 16:12:50 +0530

    Added Security Config from Spring Boot Starter Security

---
+++ src/main/java/com/himanshu/journalApp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.himanshu.journalApp.services;
+
+import com.himanshu.journalApp.entities.User;
+import com.himanshu.journalApp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if (user != null) {
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getUserName())
+                    .password(user.getPassword())
+                    .roles(user.getRoles().toArray(new String[0]))
+                    .build();
+        }
+        throw new UsernameNotFoundException(String.format("The user with username %s was not found.", username));
+    }
+}
