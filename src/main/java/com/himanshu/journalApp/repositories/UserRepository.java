458e207747f9c6713b877d7348b17e6a6d50031e
hmnhGeek <hmnhsharma97@gmail.com>
2025-01-28 12:55:46 +0530

    Added User class and User repository class

---
+++ src/main/java/com/himanshu/journalApp/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.himanshu.journalApp.repositories;
+
+import com.himanshu.journalApp.entities.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+}
---
+++ src/main/java/com/himanshu/journalApp/services/UserService.java
@@ -0,0 +1,57 @@
+package com.himanshu.journalApp.services;
+
+import com.himanshu.journalApp.entities.JournalEntry;
+import com.himanshu.journalApp.entities.User;
+import com.himanshu.journalApp.repositories.UserRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Component
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     *
+     * @param user Parameter of type {@code User}.
+     * @return {@code User} by saving the entry inside the MongoDB collection.
+     */
+    public User save(User user) {
+        User savedJournalEntry = userRepository.save(user);
+        return user;
+    }
+
+    /**
+     * This method returns all the saved users from the collection.
+     * @return The return type is {@code List<User>}.
+     */
+    public List<User> getAll() {
+        List<User> allUsers = userRepository.findAll();
+        return allUsers;
+    }
+
+    /**
+     *
+     * @param id A parameter of type {@code ObjectId} corresponding to the primary key in the mongodb collection.
+     * @return Optional user.
+     */
+    public Optional<User> getById(ObjectId id) {
+        // Optional is like a box; there can be data inside it or not.
+        Optional<User> user = userRepository.findById(id);
+        return user;
+    }
+
+    /**
+     *
+     * @param id A parameter of type {@code ObjectId} corresponding to the primary key in the mongodb
+     */
+    public void deleteById(ObjectId id) {
+        userRepository.deleteById(id);
+    }
+}
