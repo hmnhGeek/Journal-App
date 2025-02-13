8bf6024eb7bddf4398d0cbb514ead5f910b9c540
hmnhGeek <hmnhsharma97@gmail.com>
2025-02-13 21:35:09 +0530

    Parameterized testing

--- src/test/java/com/himanshu/journalApp/services/UserServiceTests.java
+++ src/test/java/com/himanshu/journalApp/services/UserServiceTests.java
@@ -1,7 +1,10 @@
 package com.himanshu.journalApp.services;
 
 import com.himanshu.journalApp.repositories.UserRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +17,22 @@ public class UserServiceTests {
     @Autowired
     private UserRepository userRepository;
 
+    @Disabled // we can disable the test also.
     @Test
     public void testFindByUserName() {
         // This user must be present in the database collection for the test to pass.
         assertNotNull(userRepository.findByUserName("ram"));
+    }
+
+    // we can use parameterized testing also
+    @ParameterizedTest
+    // either pass a CSV file or use this annotation to manually pass the arguments.
+    @CsvSource({
+            "1,1,2",
+            "2,2,8",
+            "2,3,5"
+    })
+    public void testSum(int a, int b, int c) {
+        assertEquals(c, a + b);
     }
 }
